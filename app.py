a0145348e25e61d7845beec29943e108ba0eea74
Samwalton9 <samwalton9@gmail.com>
2025-11-14 07:42:59 +0000

    Query working; local dev working

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from flask import Flask, render_template, request
 import mysql.connector
 import yaml
@@ -9,53 +10,98 @@ def load_secrets():
         secrets = yaml.load(f, Loader=yaml.FullLoader)
     return secrets
 
+def date_as_mw_timestamp(date_string):
+    return date_string.strftime("%Y%m%d") + "000000"
+
 @app.route('/', methods=['GET', 'POST'])
 def homepage():
     secret_info = load_secrets()
-    context = {
-        'edit_count_min': 0,
-    }
+    context = {}
 
     if request.method == 'POST':
         database = request.form.get('database')
         edit_count_min = request.form.get('edit-count-min')
         edit_count_max = request.form.get('edit-count-max')
+        account_age_min = request.form.get('account-age-min')
+        account_age_max = request.form.get('account-age-max')
 
         context['database'] = database
         context['edit_count_min'] = edit_count_min
         context['edit_count_max'] = edit_count_max
-        print(context)
+        context['account_age_min'] = account_age_min
+        context['account_age_max'] = account_age_max
 
-        db_connection = mysql.connector.connect(
-            host=f"{database}.analytics.db.svc.wikimedia.cloud",
-            user=secret_info['toolforge_user'],
-            password=secret_info['toolforge_password'],
-            database=f"{database}_p"
-        )
+        # Requires SSH tunnel per
+        # https://wikitech.wikimedia.org/wiki/Help:Wiki_Replicas#Connecting_to_the_database_replicas_from_your_own_computer
+        if app.config['DEBUG']:
 
-        # Connecting to the replicas is a pain locally, so use mock data locally
-        # and a real database connection & query if we're on Toolforge.
-        if app.config['FLASK_ENV'] == 'development':
-            pass
-        elif app.config['FLASK_ENV'] == 'production':
-            cursor = db_connection.cursor()
+            db_connection = mysql.connector.connect(
+                host="127.0.0.1",
+                user=secret_info['toolforge_user'],
+                password=secret_info['toolforge_password'],
+                database=f"{database}_p"
+            )
 
-            cursor.execute(f"""
-                SELECT DISTINCT
-                    actor.actor_name AS user_name,
-                    user.user_editcount
-                FROM user
-                JOIN actor ON actor.actor_user = user.user_id
-                JOIN revision ON revision.rev_actor = actor.actor_id
-                WHERE revision.rev_timestamp > 20251013000000
-                AND user.user_editcount BETWEEN {edit_count_min} AND {edit_count_max}
-                AND user.user_is_temp = False
-                ORDER BY user.user_editcount DESC;
-            """)
+        elif not app.config['DEBUG']:
 
-            user_list = cursor.fetchall()
+            db_connection = mysql.connector.connect(
+                host=f"{database}.analytics.db.svc.wikimedia.cloud",
+                user=secret_info['toolforge_user'],
+                password=secret_info['toolforge_password'],
+                database=f"{database}_p"
+            )
+
+        edit_count_min_query = ""
+        if edit_count_min:
+            edit_count_min_query = f"AND user.user_editcount > {edit_count_min}"
+
+        edit_count_max_query = ""
+        if edit_count_max:
+            edit_count_max_query = f"AND user.user_editcount < {edit_count_max}"
+
+        now = datetime.now()
+
+        account_age_min_query = ""
+        if account_age_min:
+            account_min_date = now - timedelta(days=int(account_age_min))
+            account_min_date_mw = date_as_mw_timestamp(account_min_date)
+            account_age_min_query = f"AND user.user_registration < {account_min_date_mw}"
+
+        account_age_max_query = ""
+        if account_age_max:
+            account_max_date = now - timedelta(days=int(account_age_max))
+            account_max_date_mw = date_as_mw_timestamp(account_max_date)
+            account_age_max_query = f"AND user.user_registration > {account_max_date_mw}"
+
+        one_week_ago = now - timedelta(days=7)
+        one_week_ago_timestamp = date_as_mw_timestamp(one_week_ago)
+
+        query = f"""
+            SELECT DISTINCT
+                actor.actor_name AS user_name,
+                user.user_editcount
+            FROM user
+            JOIN actor ON actor.actor_user = user.user_id
+            JOIN revision ON revision.rev_actor = actor.actor_id
+            WHERE revision.rev_timestamp > {one_week_ago_timestamp}
+            AND user.user_is_temp = False
+            {edit_count_min_query}
+            {edit_count_max_query}
+            {account_age_min_query}
+            {account_age_max_query}
+            ORDER BY user.user_editcount DESC;
+        """
+
+        cursor = db_connection.cursor()
+
+        cursor.execute(query)
+
+        user_list = cursor.fetchall()
+
+        # TODO: Return a CSV
 
         context['user_list'] = user_list
+        context['query'] = query
 
     return render_template('index.html', **context)
 
